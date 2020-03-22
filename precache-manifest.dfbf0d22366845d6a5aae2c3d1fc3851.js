4860d3de4dc5293eeb6180ed837a0eed3b9b874c
akhinchey <akhinchey@gmail.com>
2020-03-21 22:09:06 -0400

    Updates

---
+++ precache-manifest.dfbf0d22366845d6a5aae2c3d1fc3851.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9892d7509a0489fbba2a235221e0927",
+    "url": "/gem-search/index.html"
+  },
+  {
+    "revision": "9ea2aeb0cb8f57bab1ed",
+    "url": "/gem-search/static/js/2.a3f92899.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/gem-search/static/js/2.a3f92899.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf03d4f8c00ae98ccb1c",
+    "url": "/gem-search/static/js/main.5c72f514.chunk.js"
+  },
+  {
+    "revision": "9d2e0991a5ddc16032d6",
+    "url": "/gem-search/static/js/runtime-main.39539638.js"
+  }
+]);
